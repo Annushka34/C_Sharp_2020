c33b0ae84aa8bb5f4c960c6f851b76c38ae156ea
annushka34 <annsamoluk82@gmail.com>
2020-09-27 12:31:28 +0200

    non generic

---
+++ Lessn_6_Collection/Hashtable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace Hashtable_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hash = new Hashtable();
+           // SortedList hash = new SortedList();
+           //  едементи вставляються в відсотрованому порядку і мають бути одного типу
+
+
+            hash.Add(10, "Ivanov");
+            hash.Add(15, "Petrov");
+          //  hash.Add(10, "Ivanov"); - exception
+            hash[10] = "Sidorov";//  replace value
+            hash.Add('a', "Pupkin");
+
+            foreach (var key in hash.Keys)
+            {
+                Console.WriteLine(key + ":" + hash[key]);
+            }
+            Console.WriteLine("-----------------------------------");
+            foreach (var val in hash.Values)
+            {
+                Console.WriteLine(val);
+            }
+
+            hash.Remove(15);
+            
+
+            Console.WriteLine("-----------------------------------");
+            foreach (var val in hash.Values)
+            {
+                Console.WriteLine(val);
+            }
+            //hash.Add(new MyKey(), 23);
+            //hash.Add(new MyKey(), 23);
+            //hash.Add(new MyKey(), 23);
+
+           
+        }
+    }
+
+    class MyKey
+    {
+        string name;
+    }
+}
---
+++ Lessn_6_Collection/Lessn_6_Collection/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace Lessn_6_Collection
+{
+    class Program
+    {
+        //  no generic collections
+        //ArrayList
+        //BitArray
+        //HashtaЬle
+        //Queue
+        //SortedList
+        //Stack
+
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList(new int[] { 3, 5, 7 });// ICollection
+            ArrayList arr2 = new ArrayList(5);//capacity
+            //arr2.AddRange(arr);
+            //arr2.AddRange(new int[] { 2, 8});
+           // arr2.Add("hello");
+
+            //foreach (var item in arr2)
+            //{
+            //    System.Console.WriteLine(item);
+            //}
+            //System.Console.WriteLine(arr2.Capacity);
+            //arr2.TrimToSize();
+            //Console.WriteLine(arr2.Capacity);
+
+            Console.WriteLine(arr2.Contains("hello"));
+            Console.WriteLine(arr2.IndexOf("hello"));
+
+           // arr2.Remove("hello");
+            Console.WriteLine("-------------------------------");
+
+            foreach (var item in arr2)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            Car car1 = new Car("BMW");
+            Car car2 = new Car("Mersedes");
+            arr2.Add(car1);
+            arr2.Add(car2);
+            Console.WriteLine("-------------------------------");
+            foreach (var item in arr2)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //arr2.Remove(car1);
+            Console.WriteLine("-------------------------------");
+            arr2.Sort();
+            foreach (var item in arr2)
+            {
+                System.Console.WriteLine(item);
+            }
+            var element = (Car)arr2[1];            
+        }
+    }
+
+    class Car : IComparable
+    {
+        public Car(string n)
+        {
+            name = n;
+        }
+        public string name;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.name.CompareTo(((Car)obj).name);
+        }
+    }
+}
