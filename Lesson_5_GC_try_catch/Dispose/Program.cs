3d948176d2fbd14c435412b1c1199f54528a3d64
annushka34 <annsamoluk82@gmail.com>
2020-09-27 11:02:29 +0200

    Dispose

---
+++ Lesson_5_GC_try_catch/Dispose/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person p = new Person();
+            p.Dispose();
+            p.Dispose();
+
+            using (Person p1 = new Person())
+            {
+                //   work with person
+            }
+
+            
+
+            using (FileStream fs = new FileStream("dvsd.txt", FileMode.OpenOrCreate))
+            {
+
+            }
+        }
+    }
+
+
+    class Person : IDisposable
+    {
+        FileStream fileStream;
+        public Person()
+        {
+            fileStream = new FileStream("D://new.txt", FileMode.OpenOrCreate);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose");
+            fileStream.Close();
+            fileStream.Dispose();
+        }
+    }
+}
---
+++ Lesson_5_GC_try_catch/GC/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GC_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GetTotalMemory: " + GC.GetTotalMemory(false));
+            Person[] person = new Person[100];
+            Console.WriteLine("CollectionCount 0: " + GC.CollectionCount(0));
+            Console.WriteLine("CollectionCount 1: " + GC.CollectionCount(1));
+            Console.WriteLine("CollectionCount 2: " + GC.CollectionCount(2));
+
+            for (int i = 0; i < 100; i++)
+            {
+               // Person p = new Person();
+                person[i] = new Person();
+               // Console.WriteLine("GetTotalMemory: " + GC.GetTotalMemory(false));
+               // GC.Collect();
+               // Console.WriteLine("GetGeneration: " + GC.GetGeneration(p));              
+            }
+
+
+            Console.WriteLine("CollectionCount 0: " + GC.CollectionCount(0));
+            Console.WriteLine("CollectionCount 1: " + GC.CollectionCount(1));
+            Console.WriteLine("CollectionCount 2: " + GC.CollectionCount(2));
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    person[i].Foo(i);
+            //}          
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public int [] i = new int[100000];
+        public void Foo(int x)
+        {
+           
+        }
+
+        ~ Person()//  фіналізатор
+        {
+            Console.WriteLine("I will die... clear resources");
+        }
+    }
+
+}
